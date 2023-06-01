00f9cc0121c677a11a2991640f01a5006cb7b94d
Влад Іванов <vlad.iva.od@gmail.com>
2023-06-01 14:30:10 +0300

    Add list support

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import time
-reg_num = input("Введите регистрационный номер: ")
+list_of_reg_num = ['Владимир Петрович', 'Мария Иванова', 'Андрей Осипенко']
+sel_num = input("Выберите водителя из списка: " + str(list_of_reg_num) + " ")
+if sel_num in list_of_reg_num:
+    print("Сейчас вы смотрите информацию по водителю: " + sel_num)
+    time.sleep(0.5)
+else:
+    print("Такого водителя не существует, попробуйте заново")
+    exit()
 dohod = int(input("Чистый доход водителя в кроннах: "))
 bonus = int(input("Введите колиство бонусов(будут поделены на 50%): "))
 orenda = int(input("Введите затраты на аренду: "))
 time.sleep(0.5)
 print("Также будут вычтены 700 кронн за гараж")
 time.sleep(1)
-print("Регистрационный номер по которму вы смотрите информацию: " + reg_num)
 def present():
     result = dohod - (bonus * 0.5)
     amount = result - (orenda - 700)
