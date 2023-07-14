86b135de5b799aac0024694581c0408adf089a0d
Влад Іванов <vlad.iva.od@gmail.com>
2023-07-14 11:11:47 +0300

    Creating adding mode which allows users to add a new users in list(alpha test). Creating a new dialog window for the adding mode

--- interface.py
+++ interface.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QComboBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QComboBox, QDialogButtonBox, QDialog, QTextBrowser
+from PyQt5.QtGui import QIcon
 from main import save_users
 from main import load_users
 from main import users
@@ -7,12 +8,32 @@ from main import users
 app = QApplication(sys.argv)
 
 window = QMainWindow()
-
 window.setFixedSize(800, 600)
-window.setWindowTitle("Cab Manager")
+window.setWindowTitle("Taxi Manager")
+app_icon = QIcon ("Icon.png")
+window.setWindowIcon(app_icon)
 window.resize(800, 600)
 window.show()
 
+def adding_mode():
+    dialog = QDialog()
+    dialog.resize(248,229)
+    sign = QTextBrowser(dialog)
+    sign.setText("Вы точно хотите добавить нового пользователя?")
+    sign.show()
+    sign.setGeometry(30,80,201,51)
+    sign.resize(180,40)
+    button_box = QDialogButtonBox(dialog)
+    button_box.setStandardButtons(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+    button_box.show()
+    button_box.setGeometry(45,140,156,21)
+    def Ok_canl(button):
+        if button == button_box.button(QDialogButtonBox.Ok):
+            print("Hello World!")
+        elif button == button_box.button(QDialogButtonBox.Cancel):
+            print("Hello World!")
+    button_box.clicked.connect(Ok_canl)
+    dialog.exec_()
 combobox = QComboBox(window)  # Создаем пустой QComboBox с родителем window
 combobox.setGeometry(10, 1, 351, 51)
 
@@ -21,7 +42,9 @@ for user in users:
 
 add_user = QPushButton("Добавить нового пользователя", window) #Создаём кнопку которая позволяет добавить нового пользователя
 add_user.setGeometry(370, 0, 191, 51)
-
+add_user.clicked.connect(adding_mode)
+    
+    
 del_user = QPushButton("Удалить пользователя", window)
 del_user.setGeometry(570, 0, 211, 51)
 
